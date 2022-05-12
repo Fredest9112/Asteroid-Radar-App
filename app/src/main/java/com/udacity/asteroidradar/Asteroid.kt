aae6a7a23a04992d6ec507d263db98f41f687ae4
fredest <fredest9112@gmail.com>
2022-05-12 18:30:31 -0500

    General update to Asteroid App forked from udacity

--- app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -1,10 +1,10 @@
 package com.udacity.asteroidradar
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
                     val absoluteMagnitude: Double, val estimatedDiameter: Double,
                     val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+                    val isPotentiallyHazardous: Boolean): Parcelable
