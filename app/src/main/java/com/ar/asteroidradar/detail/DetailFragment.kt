ebc54e9b92a2f7aba39451f689913dcf30861b8c
fredest <71901890+Fredest9112@users.noreply.github.com>
2025-04-09 18:48:32 -0500

    adjusting accessibility and further gradle configs

--- app/src/main/java/com/ar/asteroidradar/detail/DetailFragment.kt
+++ app/src/main/java/com/ar/asteroidradar/detail/DetailFragment.kt
@@ -29,8 +29,8 @@ class DetailFragment : Fragment() {
 
     private fun displayAstronomicalUnitExplanationDialog() {
         val builder = AlertDialog.Builder(requireActivity())
-            .setMessage(getString(R.string.astronomica_unit_explanation))
-            .setPositiveButton(android.R.string.ok, null)
+            .setMessage(getString(R.string.astronomical_unit_explanation))
+            .setPositiveButton(R.string.ok_button_dialog, null)
         builder.create().show()
     }
 }
