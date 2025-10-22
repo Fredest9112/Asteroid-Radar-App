aeb6379a76726769e370836ed3622068bc06ae25
fredest <71901890+Fredest9112@users.noreply.github.com>
2025-10-22 16:27:04 -0500

    fixing ui issue regarding compatibility with sdh 35+

--- app/src/main/java/com/ar/asteroidradar/main/MainActivity.kt
+++ app/src/main/java/com/ar/asteroidradar/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.ar.asteroidradar.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
 import com.ar.asteroidradar.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragContainerView = findViewById<FragmentContainerView>(R.id.nav_host_fragment)
+        ViewCompat.setOnApplyWindowInsetsListener(fragContainerView) { view, insets ->
+            val innerPadding = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or
+                        WindowInsetsCompat.Type.displayCutout()
+            )
+            fragContainerView.setPadding(
+                innerPadding.left,
+                innerPadding.top,
+                innerPadding.right,
+                innerPadding.bottom
+            )
+            insets
+        }
     }
 }
